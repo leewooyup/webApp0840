416ccc1724af371dfd592a75401c1c80be172eb4
leewooyup <zjadlspun1114@naver.com>
2022-09-10 16:44:20 +0900

    7th commit: Feat: 로그인 기능 구현

--- WebApp0840/src/main/java/cz/yeo/wage/WebApp0840/app/base/SecurityConfig.java
+++ WebApp0840/src/main/java/cz/yeo/wage/WebApp0840/app/base/SecurityConfig.java
@@ -1,18 +1,26 @@
 package cz.yeo.wage.WebApp0840.app.base;
 
+import cz.yeo.wage.WebApp0840.app.user.UserSecurityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 // # SpringSecurty 사용을 위한 설정
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserSecurityService userSecurityService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // # 모든 권한을 풀어준다, 로그인 안해도 됨
@@ -25,7 +33,9 @@ public class SecurityConfig {
                 .and()
                 .formLogin()
                 .loginPage("/user/login")
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .failureUrl("/user/login?error");
+
 
         return http.build();
     }
@@ -33,5 +43,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- WebApp0840/src/main/java/cz/yeo/wage/WebApp0840/app/user/repository/UserRepository.java
+++ WebApp0840/src/main/java/cz/yeo/wage/WebApp0840/app/user/repository/UserRepository.java
@@ -3,6 +3,10 @@ package cz.yeo.wage.WebApp0840.app.user.repository;
 import cz.yeo.wage.WebApp0840.app.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Integer> {
     boolean existsByLoginId(String loginId);
+
+    Optional<SiteUser> findByLoginId(String username);
 }
