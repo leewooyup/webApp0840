a60074cf6d7d772b276e6d67df1c14c0e3fe67e0
leewooyup <zjadlspun1114@naver.com>
2022-10-16 13:58:20 +0900

    29th commit: Style: 메인페이지 추가 구현 및 CSS 조정

--- WebApp0840/src/main/java/cz/yeo/wage/WebApp0840/app/home/controller/HomeController.java
+++ WebApp0840/src/main/java/cz/yeo/wage/WebApp0840/app/home/controller/HomeController.java
@@ -1,11 +1,13 @@
 package cz.yeo.wage.WebApp0840.app.home.controller;
 
 import cz.yeo.wage.WebApp0840.app.user.dto.UserContext;
+import cz.yeo.wage.WebApp0840.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +22,8 @@ public class HomeController {
     // # 루트로 접근 (return: home/main)
     @PreAuthorize("isAuthenticated()")
     @RequestMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("now", Util.date.getCurrentDateFormatted("yyyy.MM.dd"));
         return "home/main";
     }
 
--- WebApp0840/src/main/resources/static/main.js
+++ WebApp0840/src/main/resources/static/main.js
@@ -4,9 +4,34 @@ window.addEventListener('scroll', function() {
     let value = window.scrollY;
     console.log("scrollY: ", value);
 
-    if(value > 475) {
+    if(value > 395) {
         mainText.style.animation="disappear-slide 1s ease-out forwards";
     } else {
         mainText.style.animation="appear-slide 1s ease-out";
+    }
+});
+
+let dateText = document.querySelector(".current-date");
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+
+    if(value > 786) {
+        dateText.style.animation="appear-right-slide 1s ease-out";
+    } else {
+        dateText.style.animation="disappear-right-slide 1s ease-out forwards";
+    }
+});
+
+
+let minimumText = document.querySelector(".minimum-wage");
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+
+    if(value > 880) {
+        minimumText.style.animation="appear-right-slide2 1s ease-out";
+    } else {
+        minimumText.style.animation="disappear-right-slide2 1s ease-out forwards";
     }
 });
